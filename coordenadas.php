0af43d723de4fd5f9a225ff0f58c34b6e92ab829
Adrifdm <92150364+Adrifdm@users.noreply.github.com>
2023-03-09 16:45:46 +0100

    Añadidos archivos mapa sin unificar con el resto

---
+++ coordenadas.php
@@ -0,0 +1,48 @@
+<?php
+if (empty($_GET["categoria"])) {
+    exit("No hay categoría");
+}
+
+//generamos dos numeros aleatorios entre 0 y 0.01 para generar coordenadas aleatorias dentro de un rango de maximo 1,11km de 
+//distancia de la ubicacion real de los usuarios, tanto en el eje x como en el y
+mt_srand (time());
+$randomX = mt_rand(0,10) / 1000;
+$randomY = mt_rand(0,10) / 1000;
+
+$categoria = $_GET["categoria"];
+#TODO: hacerlo en una BD
+#estas no se muestran, pero esta por si se quieren poner categorias en algun momento 
+#se podrían usar para diferenciar entre todos los usuarios (pizzerias en este ejemplo) del usuario que accede a la app (veterinarias, que ahora mismo no se muestra)
+$current_user = [
+    [
+        "latitud" => 40.451722 + $randomX,
+        "longitud" => -3.738389 + $randomY,
+    ],
+];
+#estas son las posiciones que se muestran
+$users = [
+    [
+        "latitud" => 40.424312 + $randomX,
+        "longitud" => -3.712700 + $randomY,
+    ],
+    [
+        "latitud" => 40.449351 + $randomX,
+        "longitud" => -3.7217124725984663 + $randomY,
+    ],
+    [
+        "latitud" => 40.459735 + $randomX, 
+        "longitud" =>  -3.689687 + $randomY,
+    ],
+];
+
+if ($categoria === "current_user") {
+    echo json_encode([
+        "icono" => "current_user.png",
+        "coordenadas" => $current_user,
+    ]);
+} else {
+    echo json_encode([
+        "icono" => "persona.png",
+        "coordenadas" => $users,
+    ]);
+}
