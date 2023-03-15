dad93ad49ea8083d661638c69a445b6fb5b55b14
Claraf05 <claraf05@ucm.es>
2023-03-15 18:38:58 +0100

    cambios variables de sesion

--- app/views/logout.php
+++ app/views/logout.php
@@ -1,10 +1,14 @@
 <?php
 	session_start();
 
+    //limpiamos variables de sesion
+
     $_SESSION["login"] = false;
     $_SESSION["SesionEmail"] = null;
+    $_SESSION["role"] = null;
 
     session_destroy();
+
     header('Location: login.php');       
     
 ?>
