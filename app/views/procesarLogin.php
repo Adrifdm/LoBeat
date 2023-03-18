0d34632a2a190ae8fcbf5e27f93837742e01ea1c
MAGNIFI88 <sergiovargasventura@yahoo.es>
2023-03-18 14:21:26 +0100

    e

--- app/views/procesarLogin.php
+++ app/views/procesarLogin.php
@@ -1,71 +1,72 @@
 <html lang="en" >
 
-    <head>
-        <title>Login</title>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <link href="../../public/assets/css/reg_log.css" rel="stylesheet">
-    </head>
+  <head>
+    <title>Login</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link href="../../public/assets/css/reg_log.css" rel="stylesheet">
+  </head>
 
-    <body>
+  <body>
+<?php
 
-        <?php
-        session_start();
+session_start();
 
-        require_once '../controllers/usuarioController.php';
+require_once '../controllers/usuarioController.php';
 
-        // Comprobamos si el formulario ha sido enviado
-        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-            
-            // Crear una instancia de UsuarioController
-            $usuarioController = new UsuarioController();
+// Crear una instancia de UsuarioController
 
-            // Obtenemos la informacion introducida
-            $correo = $_POST['email'];
-            $contrasenya = $_POST['password'];
+$usuarioController = new UsuarioController();
 
-            // Comprobamos si existe algún usuario con ese correo
-            $usuarioExistente = $usuarioController->buscarUsuarioPorCampo('correo', $correo);
-            
-            if ($usuarioExistente !== null) {
+// Comprobamos si el formulario ha sido enviado
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    
+    // Obtenemos la informacion introducida
+    $correo = $_POST['email'];
+    $contrasenya = $_POST['password'];
 
-                // Comprobamos si se ha introducido la contraseña correcta
-                $contrasenyaCorrecta = $usuarioExistente->getContrasenya();
+    // Comprobamos si existe algún usuario con ese correo
+    $usuarioExistente = $usuarioController->buscarUsuarioPorCampo('correo', $correo);
+    
+    if ($usuarioExistente !== null) {
 
-                if ($contrasenya != $contrasenyaCorrecta) {
-                    ?>
-                    <div class = "error">
-                        <p>La contraseña introducida no es correcta </p>
-                    </div>
-                    <?php
-                    exit;
-                }
+        // Comprobamos si se ha introducido la contraseña correcta
+        $contrasenyaCorrecta = $usuarioExistente->getContrasenya();
 
-                //CONFIGURACION DE VARIABLES DE SESION  
-
-                //para ver si el usuario esta logeado durante el resto de la aplicacion
-                $_SESSION["login"] = true;
-            
-                //lo que vamos a usar durante el resto de la aplicacion para buscar info del usuario loggeado
-                $_SESSION["SesionEmail"] = $correo;
-
-                //para ver si el usuario puede acceder a pestañas dedicadas de roles especiales
-                $_SESSION["role"] = $usuarioExistente->getRole();
-
-                //header('Location: pagPrincipal.php');       
-                echo "<script>window.location='pagPrincipal.php';</script>";
-                exit;
-
-            } else {
-                ?>
-                <div class = "error">
-                    <p> El correo es inválido</p>
-                </div>
-                    <?php
-                exit;        
-            }
-
+        if ($contrasenya != $contrasenyaCorrecta) {
+            ?>
+            <div class = "error">
+                <p>La contraseña introducida no es correcta </p>
+            </div>
+            <?php
+            exit;
         }
+
+        //CONFIGURACION DE VARIABLES DE SESION  
+
+        //para ver si el usuario esta logeado durante el resto de la aplicacion
+        $_SESSION["login"] = true;
+    
+        //lo que vamos a usar durante el resto de la aplicacion para buscar info del usuario loggeado
+        $_SESSION["SesionEmail"] = $correo;
+
+        //para ver si el usuario puede acceder a pestañas dedicadas de roles especiales
+        $_SESSION["role"] = $usuarioExistente->getRole();
+
+        //header('Location: pagPrincipal.php');       
+        echo "<script>window.location='pagPrincipal.php';</script>";
+        exit;
+
+    } else {
         ?>
-    </body>
+        <div class = "error">
+            <p> El correo es inválido</p>
+        </div>
+            <?php
+        exit;        
+    }
+
+}
+?>
+</body>
 </html>
