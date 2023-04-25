ba8e846b7ce8c54eee8acb61a4630056de818070
Claraf05 <claraf05@ucm.es>
2023-04-25 12:11:07 +0200

    pag admin en proceso

--- app/views/admins/manageUsers.php
+++ app/views/admins/manageUsers.php
@@ -9,24 +9,22 @@ $usuarioController = new UsuarioController();
 $rolAdmin = false;
 
 //$spotifyController = new SpotifyController();
-if(($_SESSION["is_logged"] == true)){
-    
+if (($_SESSION["is_logged"] == true)) {
+
     $usuarioExistente = $usuarioController->obtenerUsuarioPorId($_SESSION["logged_user_id"]);
-    
-    if($usuarioExistente->getRole() == 'Admin'){
-        
+
+    if ($usuarioExistente->getRole() == 'Admin') {
+
         $rolAdmin = true;
-    }
-    else{
+    } else {
 
         $rolAdmin = false;
     }
-
 }
 
-if(($_SESSION["is_logged"] != true) || ($rolAdmin == false)){
-    
-    header('Location: ../perfil/logout.php'); 
+if (($_SESSION["is_logged"] != true) || ($rolAdmin == false)) {
+
+    header('Location: ../perfil/logout.php');
 
     exit;
 }
@@ -42,9 +40,7 @@ else if($_SESSION["logged_user_role"] != 'Admin'){
 
     <?php } 
 
-}*/
-
-else{
+}*/ else {
 
     //logica si conseguiste entrar en la pagina
 
@@ -56,25 +52,110 @@ else{
 
 <!DOCTYPE html>
 
-<html lang="en" >
-  
-    <head>
+<html lang="en">
 
-        <meta charset="UTF-8">
-        <title> Gestión de usuarios </title>
+<head>
 
-        <link rel="stylesheet" href="../../../public/assets/css/adminActions.css">
+    <meta charset="UTF-8">
+    <title> Gestión de usuarios </title>
 
-        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <link rel="stylesheet" href="../../../public/assets/css/adminActions.css">
 
-        <link rel="stylesheet" href="../../../public/assets/css/cabecera.css">
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
-        <link rel="stylesheet" href="../../../public/assets/css/notifications.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 
-        <script src="https://kit.fontawesome.com/e2e2d067dc.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="../../../public/assets/css/cabecera.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
 
-        
-    </head>
+    <link rel="stylesheet" href="../../../public/assets/css/notifications.css">
 
-  
+    <script src="https://kit.fontawesome.com/e2e2d067dc.js" crossorigin="anonymous"></script>
+
+
+</head>
+
+<body>
+    <!--
+        <div class="side-menu">
+            <div class="brand-name">
+                <h1> Brand </h1>
+            </div>
+            <ul>
+                <li>Crear Usuario</li>
+                <li>Students</li>
+                <li>Settings</li>
+                <li>Help</li>
+
+            </ul>
+        </div>
+        -->
+    <div class="containerAdmin">
+
+        <div class="headerAdmin">
+            <div class="nav">
+                <div class="search">
+                    <input type="text" placeholder="Buscar..">
+                    <button type="submit" class="fa fa-search">
+
+                    </button>
+                </div>
+                <div class="usuInfo">
+                    <a href="#" class="btnn"> Crear Usuario </a>
+                </div>
+            </div>
+        </div>
+
+        <div class="contenidoPag">
+            <div class="cardss">
+
+                <div class="cardD">
+                    <div class="boxx">
+                        <h1>21234</h1>
+                        <h3>Usuarios</h3>
+                    </div>
+                    <div class="iconCas">
+                        <img src="../../../public/assets/img/rating.png" alt="">
+                    </div>
+                </div>
+
+                <div class="cardD">
+                    <div class="boxx">
+                        <h1>21234</h1>
+                        <h3>Administradores</h3>
+                    </div>
+                    <div class="iconCas">
+                        <img src="../../../public/assets/img/rating.png" alt="">
+                    </div>
+                </div>
+
+                <div class="cardD">
+                    <div class="boxx">
+                        <h1>21234</h1>
+                        <h3> Playlists</h3>
+                    </div>
+                    <div class="iconCas">
+                        <img src="../../../public/assets/img/rating.png" alt="">
+                    </div>
+                </div>
+
+                <div class="cardD">
+                    <div class="boxx">
+                        <h1>21234</h1>
+                        <h3>Students</h3>
+                    </div>
+                    <div class="iconCas">
+                        <img src="../../../public/assets/img/rating.png" alt="">
+                    </div>
+                </div>
+
+
+            </div>
+            <div class="content-2">
+
+
+            </div>
+        </div>
+    </div>
+
+</body>
+
 </html>
