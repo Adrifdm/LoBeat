6aa052090ef34fd7046cfe75a31b67501589b177
MAGNIFI88 <sergiovargasventura@yahoo.es>
2023-05-04 01:22:16 +0200

    Incorporando el chat a la pagPrincipal

--- app/views/principal/pagPrincipal.php
+++ app/views/principal/pagPrincipal.php
@@ -2,32 +2,42 @@
 	session_start();
 
     if($_SESSION["is_logged"] != true){
-    
         header('Location: ../perfil/logout.php'); 
-        
         exit;
     }
 
-    // Lógica de los botones de la visualización de un usuario
-    if (isset($_POST['cerrarUsuario'])) {
-        $_SESSION['vista'] = 'lista';
-        header('Location: ' . $_SERVER['PHP_SELF']);
-        exit;
-    }
-    
-    if (isset($_POST['abrirChat'])) {
-        $_SESSION['vista'] = 'chat';
-    }
+    if ($_SESSION['vista'] == 'usuario') {
 
-    $nombre = null;
-    $genero = null;
-    $descripcion = null;
-    if (isset($_GET['nombre']) && isset($_GET['nombre']) && isset($_GET['nombre'])) {
-        $nombre = $_GET['nombre'];
-        $genero = $_GET['genero'];
-        $descripcion = $_GET['descripcion'];
+        $foto = null;
+        $nombre = null;
+        $genero = null;
+        $descripcion = null;
+        $matchist = null;
+
+        // Obtención información usuario seleccionado
+        if (isset($_GET['nombre']) && isset($_GET['nombre']) && isset($_GET['nombre'])) {
+            //TODO: foto
+            $nombre = $_GET['nombre'];
+            $genero = $_GET['genero'];
+            $descripcion = $_GET['descripcion'];
+            //TODO: matchlist
+        }
         
-
+        // Lógica de los botones de la visualización de un usuario
+        if (isset($_POST['cerrarUsuario'])) {
+            $_SESSION['vista'] = 'lista';
+            header('Location: ' . $_SERVER['PHP_SELF']);
+            exit;
+        }
+        if (isset($_POST['abrirChat'])) {
+            $_SESSION['vista'] = 'chat';
+        }
+    }
+    else if ($_SESSION['vista'] == 'lista') {
+        //TODO
+    }
+    else if($_SESSION['vista'] == 'chat') {
+        //TODO
     }
 ?>
 
@@ -42,8 +52,6 @@
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@10.16.6/dist/sweetalert2.min.css">
         <link rel="stylesheet" href=" https://unpkg.com/openlayers@4.6.5/dist/ol.css">
         <link rel="stylesheet" href="../../../public/assets/css/pagPrincipal.css">
-        <link rel="stylesheet" href="../../../public/assets/css/chat.css">
-        <!-- <link rel="stylesheet" href="../../../public/assets/css/chatlist.css"> -->
         
         <!-- Scripts mapa -->
         <script type="text/javascript" src="https://unpkg.com/openlayers@4.6.5/dist/ol.js"></script>
@@ -77,11 +85,9 @@
                     <h1>
                         <?php            
                             if ($nombre != null) {
-                                
                                 echo $nombre;
-
                             }          
-                         ?>
+                        ?>
                     </h1>
                     <h2>
                         <?php                
@@ -91,8 +97,7 @@
                         ?>
                     </h2>
                     <p>
-                        <?php
-                                        
+                        <?php         
                             if ($descripcion != null) {
                                 echo $descripcion;
                             }          
@@ -396,99 +401,53 @@
             </div>
             
             <?php elseif ($_SESSION["vista"] == 'chat'): ?>
+
             <div class = "chatConcreto">
-            <div class="app-main">
+
+                <div class="seccionSuperior">
+                    SECCION SUPERIOR
+                </div>
+
                 <div class="chat-wrapper">
+
                     <div class="message-wrapper reverse">
-                        <img class="message-pp" src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2550&q=80" alt="profile-pic">
+                        <!-- <img class="message-pp" src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2550&q=80" alt="profile-pic"> -->
                         <div class="message-box-wrapper">
-                        <div class="message-box">
-                        Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur
-                        </div>
+                            <div class="message-box">
+                                MENSAJE 2
+                            </div>
                         </div>
                     </div>
+
                     <div class="message-wrapper reverse">
-                        <img class="message-pp" src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2550&q=80" alt="profile-pic">
+                        <!-- <img class="message-pp" src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2550&q=80" alt="profile-pic"> -->
                         <div class="message-box-wrapper">
-                        <div class="message-box">
-                            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                        </div>
+                            <div class="message-box">
+                                MENSAJE 2
+                            </div>
                         </div>
                     </div>
+
                     <div class="message-wrapper">
-                        <img class="message-pp" src="https://images.unsplash.com/photo-1587080266227-677cc2a4e76e?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&amp;ixlib=rb-1.2.1&amp;auto=format&amp;fit=crop&amp;w=934&amp;q=80" alt="profile-pic">
+                        <!-- <img class="message-pp" src="https://images.unsplash.com/photo-1587080266227-677cc2a4e76e?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&amp;ixlib=rb-1.2.1&amp;auto=format&amp;fit=crop&amp;w=934&amp;q=80" alt="profile-pic"> -->
                         <div class="message-box-wrapper">
-                        <div class="message-box">
-                            Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur
+                            <div class="message-box">
+                                MENSAJE 1
+                            </div>
                         </div>
-                        </div>
                     </div>
-                <div class="message-wrapper">
-                    <img class="message-pp" src="https://images.unsplash.com/photo-1587080266227-677cc2a4e76e?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&amp;ixlib=rb-1.2.1&amp;auto=format&amp;fit=crop&amp;w=934&amp;q=80" alt="profile-pic">
-                    <div class="message-box-wrapper">
-                    <div class="message-box">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit 
-                    </div>
-                    </div>
+
                 </div>
-                <div class="message-wrapper reverse">
-                    <img class="message-pp" src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2550&q=80" alt="profile-pic">
-                    <div class="message-box-wrapper">
-                    <div class="message-box">
-                        Lorem ipsum dolor sit amet
-                    </div>
-                    </div>
-                </div>
-                <div class="message-wrapper reverse">
-                    <img class="message-pp" src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2550&q=80" alt="profile-pic">
-                    <div class="message-box-wrapper">
-                    <div class="message-box">
-                        Lorem ipsum dolor
-                    </div>
-                    </div>
-                </div>
-                <div class="message-wrapper">
-                    <img class="message-pp" src="https://images.unsplash.com/photo-1587080266227-677cc2a4e76e?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&amp;ixlib=rb-1.2.1&amp;auto=format&amp;fit=crop&amp;w=934&amp;q=80" alt="profile-pic">
-                    <div class="message-box-wrapper">
-                    <div class="message-box">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit
-                    </div>
-                    </div>
-                </div>
-                <div class="message-wrapper">
-                    <img class="message-pp" src="https://images.unsplash.com/photo-1587080266227-677cc2a4e76e?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&amp;ixlib=rb-1.2.1&amp;auto=format&amp;fit=crop&amp;w=934&amp;q=80" alt="profile-pic">
-                    <div class="message-box-wrapper">
-                    <div class="message-box">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit
-                    </div>
-                    </div>
-                </div>
-                <div class="message-wrapper">
-                    <img class="message-pp" src="https://images.unsplash.com/photo-1587080266227-677cc2a4e76e?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&amp;ixlib=rb-1.2.1&amp;auto=format&amp;fit=crop&amp;w=934&amp;q=80" alt="profile-pic">
-                    <div class="message-box-wrapper">
-                    <div class="message-box">
-                        Lorem ipsum dolor sit amet
-                    </div>
-                    </div>
-                </div>
-                <div class="message-wrapper reverse">
-                    <img class="message-pp" src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2550&q=80" alt="profile-pic">
-                    <div class="message-box-wrapper">
-                    <div class="message-box">
-                        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                    </div>
-                    </div>
-                </div>
-                </div>
+
                 <div class="chat-input-wrapper">
-                <button class="chat-attachment-btn">
-                </button>
-                <div class="input-wrapper">
-                    <input type="text" class="chat-input" placeholder="Introduzca su mensaje aqui">
+                    
+                    <button class="chat-attachment-btn"></button>
+                    <div class="input-wrapper">
+                        <input type="text" class="chat-input" placeholder="Introduzca su mensaje aqui">
+                    </div>
+                    <button class="chat-send-btn">Send</button>
+
                 </div>
-                <button class="chat-send-btn">Send</button>
-                </div>
-            </div>
             </div>
 
             <?php endif; ?>
