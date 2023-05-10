37042d9d42719b4019cf34e174046242cd94bdbc
t3rgo <miguelangeljr01@gmail.com>
2023-05-10 20:11:36 +0200

    .

--- app/matching/matchAlgorithm.php
+++ app/matching/matchAlgorithm.php
@@ -1,3 +1,19 @@
 <?php
+
+require_once '../../../app/controllers/usuarioController.php';
+require_once '../../../app/controllers/spotifyController.php';
+require_once '../../../app/controllers/playlistController.php';
+
+session_start();
+
+$usuarioController = new UsuarioController();
+$spotifyController = new SpotifyController(); 
+$playlistsController = new PlaylistsController();
+
+$user = $usuarioController->obtenerUsuarioPorId($_SESSION['logged_user_id']);
+$recently_played_info = $spotifyController->obtenerRecentlyPlayed();
+
 phpinfo();
+
+
 ?>
